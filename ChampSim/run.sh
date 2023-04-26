353d8ee08f98ad0b23cf982ef240198828bc97ff
Anish <21b090003@iitb.ac.in>
2023-04-26 14:02:41 +0530

    removing results

---
+++ ChampSim/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+bin/champsim --warmup_instructions 20000000 --simulation_instructions 50000000 ../traces/cc-14.trace.gz > ../results_20_30/cc/$1
+bin/champsim --warmup_instructions 20000000 --simulation_instructions 50000000 ../traces/sssp-14.trace.gz > ../results_20_30/sssp/$1
