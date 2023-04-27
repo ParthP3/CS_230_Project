4bb42f25700b39ba297ed595fedc831600c2d31a
Anish <21b090003@iitb.ac.in>
2023-04-27 12:01:28 +0530

    results and run

--- ChampSim/run.sh
+++ ChampSim/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-bin/champsim --warmup_instructions 20000000 --simulation_instructions 30000000 ../traces/cc-14.trace.gz > ../results_20_30/cc/$1 &
-bin/champsim --warmup_instructions 20000000 --simulation_instructions 30000000 ../traces/sssp-14.trace.gz > ../results_20_30/sssp/$1 &
+bin/champsim --warmup_instructions 30000000 --simulation_instructions 30000000 ../traces/cc-14.trace.gz > ../results/cc/$1 &
+bin/champsim --warmup_instructions 30000000 --simulation_instructions 30000000 ../traces/sssp-14.trace.gz > ../results/sssp/$1 &
+bin/champsim --warmup_instructions 30000000 --simulation_instructions 30000000 ../traces/bc-5.trace.gz > ../results/bc/$1 &
+bin/champsim --warmup_instructions 30000000 --simulation_instructions 30000000 ../traces/bfs-10.trace.gz > ../results/bfs/$1 &
